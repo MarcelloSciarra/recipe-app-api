fe40f1b807510f84c6441a836772e0312a28a168
MRSC <marcello.sciarra@deimos-space.com>
2024-06-05 09:13:58 +0200

    wip

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -52,7 +52,9 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short_error(self):
-        """Test an error is returned if password is less than 5 characters long"""
+        """Test an error is returned if password is less
+        than 5 characters long
+        """
         payload = {
             'email': 'test@example.com',
             'password': 'pw',

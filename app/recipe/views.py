657386caa755d781f26a559ebee34260a7979a35
MRSC <marcello.sciarra@deimos-space.com>
2024-06-14 13:25:08 +0200

    wip

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,17 +1,33 @@
 """
 Views for the recipe APIs.
 """
-from rest_framework import viewsets
+from rest_framework import (
+    viewsets,
+    mixins,
+)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+    Ingredient
+)
 from recipe import serializers
 
 
+# ModelViewSet is a class that has bases:
+# - mixins.CreateModelMixin
+# - mixins.RetrieveModelMixin
+# - mixins.UpdateModelMixin
+# - mixins.DestroyModelMixin
+# - mixins.ListModelMixin
+# - GenericViewSet
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
+    # This docstring above is directly translated to the online
+    # swagger documentation of the API
     serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
@@ -28,3 +44,31 @@ class RecipeViewSet(viewsets.ModelViewSet):
             if self.action == "list"
             else self.serializer_class
         )
+
+    def perform_create(self, serializer):
+        """ Create a new recipe"""
+        serializer.save(user=self.request.user)
+
+
+class BaseRecipeAttrViewSet(mixins.UpdateModelMixin,
+                            mixins.ListModelMixin,
+                            mixins.DestroyModelMixin,
+                            viewsets.GenericViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to auth user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+# This is an explicit way of specifying the required operations
+class TagViewSet(BaseRecipeAttrViewSet):
+    """Manage tags in the database"""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
